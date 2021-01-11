e1e3bade5f4aa4ef1aebbb8f1652ec8b5d6d7594
SALTUK YAŞAR <46872799+saltukyasar@users.noreply.github.com>
2021-01-12 01:48:38 +0300

    Add files via upload

---
+++ Example 4/reportingForDuty().js
@@ -0,0 +1,13 @@
+Write a function, reportingForDuty(), that has two string parameters, rank and lastName, and returns a string in the following format: ‘rank lastName reporting for duty!’
+
+// Write your function here:
+function reportingForDuty(rank ,lastName){
+  return (`${rank} ${lastName} reporting for duty!`);
+
+}
+console.log(reportingForDuty('saltuk','yasar'));
+
+// Uncomment the line below when you're ready to try out your function
+// console.log(reportingForDuty('Private', 'Fido')) // Should return 'Private Fido reporting for duty!'
+
+// We encourage you to add more function calls of your own to test your code!
