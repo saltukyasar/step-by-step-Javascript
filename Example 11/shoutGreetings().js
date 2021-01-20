76dabc6031feda03c5bbe72591db93a0cae633c6
SALTUK YAŞAR <46872799+saltukyasar@users.noreply.github.com>
2021-01-21 01:41:18 +0300

    Add files via upload

---
+++ Example 11/shoutGreetings().js
@@ -0,0 +1,22 @@
+/*
+Write a function shoutGreetings() that takes in an array of strings and returns a new array. This new array should contain all the strings from the argument array but with capitalized letters and an exclamation point appended to the end: 'heya' will become 'HEYA!'
+
+You can use any technique you want to accomplish this task.
+
+You can test your function when you’re ready by passing in the greetings array or by making your own array!
+*/
+const shoutGreetings = arr => arr.map(word => word.toUpperCase() + '!');
+
+/*
+// As a function declaration AND using a loop:
+function shoutGreetings(arr) {
+    let shoutArray = []
+    for(let i = 0; i<arr.length; i++){
+        shoutArray.push(arr[i].toUpperCase() + '!')
+    }
+    return shoutArray
+}
+*/
+const greetings = ['hello', 'hi', 'heya', 'oi', 'hey', 'yo']
+
+console.log(shoutGreetings(greetings))
