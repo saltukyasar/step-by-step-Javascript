865fc52a15c8f168d928bb34996ad99379c8a616
SALTUK YAŞAR <46872799+saltukyasar@users.noreply.github.com>
2021-01-24 05:24:25 +0300

    Add files via upload

---
+++ Example 13/justCoolStuff().js
@@ -0,0 +1,30 @@
+//Write a function justCoolStuff() that takes in two arrays of strings, and, using the built-in .filter() method, returns an array with the items that are present in both arrays.
+
+let arr1 = ['this', 'not this', 'nor this'];
+let arr2 = ['thing 1', 'thing 2', 'this'];
+ 
+justCoolStuff(arr1, arr2); // Should return ['this']
+//You can test your function when you’re ready by passing in the myStuff and coolStuff arrays or by making arrays of your own!
+
+const justCoolStuff = (firstArray, secondArray) => firstArray.filter(item => secondArray.includes(item))
+
+/*
+// As a function declaration AND using named function w/ a loop:
+function justCoolStuff(firstArray, secondArray) {
+      function isInSecondArray(item){
+            for(let i = 0; i<secondArray.length; i++){
+                  if (secondArray[i] === item){
+                        return true
+                  }
+            }
+            return false 
+      }
+      return firstArray.filter(isInSecondArray)
+}
+*/
+
+const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+
+const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
+
+console.log(justCoolStuff(myStuff, coolStuff))
